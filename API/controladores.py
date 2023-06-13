8c80c4d9ecf3e04fb6f1dadd888cb6ff19840225
DemonBrain <81323986+DemonBrain@users.noreply.github.com>
2023-06-13 01:07:30 -0400

    Update controladores.py

--- API/controladores.py
+++ API/controladores.py
@@ -60,7 +60,7 @@ def insert_sensordata(sensor_id, data_field1, data_field2, sensor_api_key):
     cursor.execute(contra, [sensor_id])
     admin_password = cursor.fetchone()[0]
 
-    if sensor_api_key == admin_password:
+    if sensor_api_key == contra:
              statement = "INSERT INTO SensorData(sensor_id, data_field1, data_field2 )  VALUES (?, ?, ?)"
              cursor.execute(statement, [sensor_id, data_field1, data_field2])
              db.commit()
@@ -185,7 +185,7 @@ def specific_sensordata(sensor_id,sensor_api_key):
     cursor.execute(contra, [sensor_id])
     admin_password = cursor.fetchone()[0]
 
-    if sensor_api_key == admin_password:
+    if sensor_api_key == contra:
              statement = "SELECT * FROM SensorData WHERE sensor_id = ?"
              cursor.execute(statement, [sensor_id])
              db.commit()
@@ -202,7 +202,7 @@ def update_sensordata( sensor_id, data_field1, data_field2, sensor_api_key):
     cursor.execute(contra, [sensor_id])
     admin_password = cursor.fetchone()[0]
 
-    if sensor_api_key == admin_password:
+    if sensor_api_key == contra:
              statement = "UPDATE SensorData SET data_field1 = ?, data_field2 = ? WHERE sensor_id = ?"
              cursor.execute(statement, [data_field1, data_field2, sensor_id])
              db.commit()
@@ -219,7 +219,7 @@ def delete_sensordata(sensor_id,sensor_api_key):
     cursor.execute(contra, [sensor_id])
     admin_password = cursor.fetchone()[0]
 
-    if sensor_api_key == admin_password:
+    if sensor_api_key == contra:
              statement = "DELETE FROM SensorData WHERE sensor_id = ?;"
              cursor.execute(statement, [sensor_id])
              db.commit()
